62150df53e78990e31e2a6fc71cb2d86900226a6
Vincent Kumwenda <vkumwenda@gmail.com>
2014-09-30 00:58:03 +0200

    commit all files

---
+++ documents/projects/rails/vince/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vince::Application.config.secret_token = '637f9aa199eb621e19e729bdb000592201f09ac82df80d3f3b88cc9505d03369d77cc4cf8feaa5efd17239a61919991f90e4f1f5638a4d04a0b1fbcf7189233c'
